f680000a64a36b8efddcb7bc221c2d3bed77209f
Rafa ✏️ <adanescollante@gmail.com>
2022-01-22 17:20:30 -0400

    fixed favicon bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
           <meta name="description" content="NFT preview app"/>
           <meta name="keywords" content="Template, Challenge, frontendmentor, NFT, NextJS"/>
           <meta name="author" content="Rafa"/>
-          <link rel="shortcut icon" href="../public/favicon.ico"/>
+          <link rel="shortcut icon" href="/favicon.ico"/>
       </Head>
       <Component {...pageProps} />
     </div>
